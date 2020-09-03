25954965be78f0014ddb31b73c4df98332a32f3a
Matthew Wei <matthewwei35@gmail.com>
2020-09-03 13:03:56 -0700

    Updated Water_Calculator.py and README.md
    Fixed punctuation in Water_Calculator.py and added more info in README.md

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CS-1.0_MW_Custom_Calculator
- My custom calculator assignment for CS 1.0
+ # Water Calculator
+ ## Description
+
+ My calculator calculates how much water you should drink daily in ounces based on your weight and the minutes of daily exercise.
+
+ This is for CS-1.0.
+
+## Usage
+```python
+weight = int(input("How much do you weigh in pounds? "))                            #Enter your weight in pounds
+exercise_time = int(input("What's your average for minutes of daily exercise? "))   #Enter your average minutes of daily exercise
+```
+
+ Programming Language: Python 3.7.9
--- Water_Calculator.py
+++ Water_Calculator.py
@@ -7,11 +7,11 @@
 print("~~~~~ Welcome to the Water Calculator! ~~~~~")
 
 #Takes user input and stores that value into the variable named weight
-weight = int(input("How much do you weigh in Pounds? "))
+weight = int(input("How much do you weigh in pounds? "))
 #Takes user input and stores that value into the variable named exercise_time
-exercise_time = int(input("What's your average for Minutes of Daily exercise? "))
+exercise_time = int(input("What's your average for minutes of daily exercise? "))
 
 #Multiply your weight by 2/3 to determine how much water to drink daily in ounces
 #Add 0.4 ounces to the total for every minute you workout daily, ex: 30 min * 0.4 = 12 ounces
 #Prints out the result of the operands weight and exercise_time
-print(">>> It is recommended that you drink", int((weight * 0.66666667) + (exercise_time * 0.4)),"ounces of water daily. <<<")
+print(">>> It is recommended that you drink", int((weight * 0.66666667) + (exercise_time * 0.4)), "ounces of water daily. <<<")
